ecc9111ef537599ff1142b1b21c2d2d00a1eacd0
rj2412 <96874201+rj2412@users.noreply.github.com>
2022-05-29 20:56:54 +0530

    Add files via upload

---
+++ Project1 - Javascript counter/index.js
@@ -0,0 +1,18 @@
+function increment()
+{
+    var num=Number(document.getElementById('number').innerHTML)
+    document.getElementById('number').innerHTML=num+1
+}
+function decrement()
+{
+    var num=Number(document.getElementById('number').innerHTML)
+    document.getElementById('number').innerHTML=num-1
+    
+
+}
+function neutral()
+{
+    var num=Number(document.getElementById('number').innerHTML)
+    document.getElementById('number').innerHTML=0
+
+}
